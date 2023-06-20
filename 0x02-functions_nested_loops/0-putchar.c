cc041489d6cc3e66126d0beda9c13dce4b344414
GeraldKhosa <gerald.khosa@gmail.com>
2023-06-20 10:07:50 -0700

    Functons

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point 
+ * discription: 'A progam that prints _putchar'
+ * return: always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('a');
+	
+	return (0);
+}
