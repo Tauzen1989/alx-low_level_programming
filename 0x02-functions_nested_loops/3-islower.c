2af29ebc364195fafb13aa6a8aa1209ed2fac4d0
GeraldKhosa <gerald.khosa@gmail.com>
2023-06-21 08:44:35 -0700

    Functions

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,8 @@
 /**
  * _islower - checks for lowercase character
  * @c: The character to be checked
- * return: 1 if character is lowercase, 0 otherwise
+ * Return: 1 if c is lowercase
+ * 0 otherwise
  */
 
 int _islower(int c)
