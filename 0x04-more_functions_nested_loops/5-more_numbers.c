6bc3a3904cf7dad4c80234c75c78b69a062a10cb
GeraldKhosa <gerald.khosa@gmail.com>
2023-06-22 06:36:59 -0700

    More functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,23 @@
 #include "main.h"
 
 /**
- * times_table - prints the 9 times table, starting with 0
+ * more_numbers - print more numbers
  *
- * return: void
+ * Return: void
  */
+
+void more_numbers(void)
+{
+	int n, m;
+
+	for (n = 1; n <= 10; n++)
+	{
+		for (m = 0; m <= 14; m++)
+		{
+			if (m >= 10)
+				_putchar('1');
+			_putchar (m % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
