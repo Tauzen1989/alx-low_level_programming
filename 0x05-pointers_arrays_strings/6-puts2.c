425139398329f9e14442a6ad42fbba5b90744aee
GeraldKhosa <gerald.khosa@gmail.com>
2023-06-26 12:46:52 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts2 - a function that prints every other character of a string,
+ * starting with the first character, 
+ * followed by a new line
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int i = 0, m =0;
+	char *k = str;
+	int n;
+
+	while (*k != '\0')
+	{
+		k++;
+		i++;
+	}
+	m = i - 1;
+	for (n = 0 ; n <= m ; n++)
+	{
+		if (n % 2 == 0)
+	{
+		_putchar(str[n]);
+	}
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts_half - a function that prints half of a string, 
+ * followed by a new line.
+ * If the number of characters is odd, 
+ * the function should print the last n characters of the string, 
+ * n = (length_of_the_string - 1) / 2
+ * @str: input string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int d, n;
+	int i = 0;
+
+	for (d = 0; str[d] != '\0'; d++)
+		i++;
+
+	n = (i / 2);
+
+	if (i % 2 == 1)
+		n = ((i + 1) / 2);
+
+	for (d = n; str[d] != '\0'; d++)
+		_putchar(str[d]);
+	_putchar('\n');
+}
