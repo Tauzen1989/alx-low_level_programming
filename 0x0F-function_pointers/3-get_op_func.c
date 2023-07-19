b7c329d5ad414b716da8c13dfa18f86dee0b846c
GeraldKhosa <gerald.khosa@gmail.com>
2023-07-19 14:03:14 -0700

    Function Pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -4,7 +4,7 @@
  * get_op_func - a function that selects the correct function
  * to perform the operation asked by the user.
  * @s: char operator.
- * Return: a pointer to the function that corresponds 
+ * Return: a pointer to the function that correspond
  * to the operator given as a parameter
  */
 int (*get_op_func(char *s))(int, int)
