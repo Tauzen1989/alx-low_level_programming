79f1450c94fa00e58a411d7317266eaa3c650f28
GeraldKhosa <gerald.khosa@gmail.com>
2023-07-28 10:48:09 -0700

    Linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -19,8 +19,8 @@ list_t *add_node(list_t **head, const char *str)
 		len++;
 	new_node->str = strdup(str);
 	new_node->len = len;
-	new_node->next = (*head);
-	(*head) = new_node;
+	new_node->next = *head;
+	*head = new_node;
 
-	return (*head);
+	return (new_node);
 }
