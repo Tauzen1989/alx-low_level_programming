9bb40a2c93e60e80fc3f84883ab254ca784f45bf
GeraldKhosa <gerald.khosa@gmail.com>
2023-08-01 01:24:39 -0700

    More linked lists

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,8 +11,8 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newNode;
-	
-	newNode = (listint_t *) malloc(sizeof(listint_t ));
+
+	newNode = (listint_t *)malloc(sizeof(listint_t));
 		newNode->n = n;
 		newNode->next = *head;
 		*head = newNode;
