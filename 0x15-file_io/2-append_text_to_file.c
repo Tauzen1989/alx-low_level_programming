d323a47267290fe0ec5ad80eda2dc6617d7baee5
GeraldKhosa <gerald.khosa@gmail.com>
2023-08-11 09:44:28 -0700

    file_io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (f_d == -1)
 		return (-1);
 	if (text_content == NULL)
-		return(1);
+		return (1);
 	len = 0;
 	while (text_content[len] != '\0')
 	{
