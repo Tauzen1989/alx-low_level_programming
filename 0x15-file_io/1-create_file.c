c3d4339f85e5fce972fa60cbe5e458bf8e229a57
GeraldKhosa <gerald.khosa@gmail.com>
2023-08-11 06:39:59 -0700

    file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,10 +14,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	f_d = open(filename, O_RDWR | O_CREAT | O_TRUNC | O_EXCL, 0600);
+	f_d = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (f_d == -1)
 	{
-		f_d = open(filename, O_RDWR | O_CREAT | O_EXCL, 0600);
+		f_d = open(filename, O_RDWR | O_CREAT, 0600);
 		if (f_d == -1)
 		{
 			return (-1);
