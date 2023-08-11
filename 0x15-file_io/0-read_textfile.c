d60adaa606c0a19e32317c75b8affb757c3a7464
GeraldKhosa <gerald.khosa@gmail.com>
2023-08-11 04:42:08 -0700

    file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,28 +16,30 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	 char *buf;
 	 ssize_t bytes_r, bytes_w;
 
+	if (filename == NULL)
+		return (0);
 	f_d = open(filename, O_RDONLY);
 	if (f_d == -1)
-		return (-1);
+		return (0);
 	buf = (char *)malloc(letters);
 	if (buf == NULL)
 	{
 		close(f_d);
-		return (-1);
+		return (0);
 	}
 	bytes_r = read(f_d, buf, letters);
 	if (bytes_r < 0)
 	{
 		free(buf);
 		close(f_d);
-		return (-1);
+		return (0);
 	}
 	bytes_w = write(STDOUT_FILENO, buf, bytes_r);
 	if (bytes_w < 0)
 	{
 		free(buf);
 		close(f_d);
-		return (-1);
+		return (0);
 	}
 
 	free(buf);
