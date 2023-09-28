5810257a4bcc5daf3e8c8d0c0d309ad71fd9e844
Tauzen1989 <gerald.khosa@gmail.com>
2023-09-28 03:39:24 -0700

    bit mainpulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,15 +8,10 @@
  */
 void print_binary(unsigned long int n)
 {
-	int bit_Count, i;
-	unsigned long int mask;
-
-	bit_Count = sizeof(unsigned long int) * 8;
-	for (i = bit_Count - 1; i >= 0; i--)
+	if (n > 1)
 	{
-		mask = 1UL << i;
-		_putchar((n & mask) ? '1' : '0');
+		print_binary(n >> 1);
 	}
 
-	_putchar('\n');
+	_putchar((n & 1) + '0');
 }
